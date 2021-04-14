ee2031e409d4bfde4d44865efa4f4f7b120e1199
min <minwang.bus@gmail.com>
2021-04-14 17:34:31 -0400

    pahse 4, sumRec

--- skeleton-11/phase_4_recursion.js
+++ skeleton-11/phase_4_recursion.js
@@ -2,10 +2,22 @@ function range(start, end) {
     if (start === end) {
         return [1];
     }
-
-    let ele = range(start + 1, end); // 
-    ele.push(start + 1); // [] << 2
+   
+    let ele = range(start, end -1)
+    ele.push(end);  
+ 
     return ele
 }
 
-console.log(range(1, 5));
+// console.log(range(1, 5));
+
+function sumRec(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    let sum = 0; 
+    sum += sumRec(arr.slice(0, arr.length -1));
+    return sum;
+}
+
+console.log(sumRec([1,5,6,3]));
