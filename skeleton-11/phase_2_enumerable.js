35f087e754537d569d4d130e812280c31cdf6536
syldysnya <syldysnya@gmail.com>
2021-04-14 16:24:00 -0400

    Finish Phase 2

--- skeleton-11/phase_2_enumerable.js
+++ skeleton-11/phase_2_enumerable.js
@@ -1,22 +1,58 @@
-Array.prototype.myEach = function(cb) {
-    const newArr = [];
+
+Array.prototype.myEach = function(callback) {
 
     for (let i = 0; i < this.length; i++) {
-        let ele = cb(this[i]);
-        newArr.push(ele);
+        callback(this[i]);
     }
-    return newArr
 }
 
-const cb = function callback(ele) {
+const foo = function(ele) {
     return ele * 10;
 }
 
-// console.log([1, 2, 3].myEach(cb));
+// console.log([1, 2, 3].myEach(callback));
 
-// Array.prototype.myMap = function() {
-//     const arr = this;
+Array.prototype.myMap = function(callback) {
 
-//     return function()
+    const arr = [];
 
-// }
+    this.myEach(function(arg) {
+        arr.push(callback(arg));
+    })
+
+    // for (let i = 0; i < this.length; i++) {
+    //     arr.push(callback(this[i]));
+    // }
+
+    return arr;
+}
+
+// console.log([1, 2, 3].myMap(ele => ele / ele));
+// console.log([1, 2, 3].myMap(foo));
+
+Array.prototype.myReduce = function(callback, initVal) {
+
+    // let acc = initVal || this[0];
+    const arr = this;
+
+    if (initVal === undefined) {
+        let initVal = arr[0];
+        let arrNew = arr.slice(1);
+
+        arrNew.myEach(function (ele) {
+            initVal += callback(ele);
+        });
+        
+        return initVal;
+    }  
+
+    let acc = initVal;
+
+    arr.myEach(function(ele) {
+        acc += callback(ele);
+    });
+
+    return acc;
+} 
+
+// console.log([1, 2, 3].myReduce(foo, 100));
